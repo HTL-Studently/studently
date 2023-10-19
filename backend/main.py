ba0fd082201b210c9ae16513ee2a50c864057276
eriksimcic <lionmaster23@protonmail.com>
2023-10-19 07:37:46 +0000

    Added further endpoints and api documentation

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 from typing import Annotated, Union
 from fastapi import Depends, FastAPI, HTTPException, status
@@ -5,19 +6,70 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
 from security import SecurityFunctions
 from db.schemas import Student, Token
 from db import dbhandler
 
+#TODO
+
+# Split API Endpoints into multiple files
+# https://fastapi.tiangolo.com/tutorial/bigger-applications/
+
+
+# API Variables
+
+load_dotenv()
+CONTACT_NAME = os.environ.get("CONTACT_NAME")
+CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL")
 
 sec = SecurityFunctions()
 db = dbhandler.DBHandler()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+description = ""
 
-app = FastAPI()
+with open("./README.md") as file:
+    description = file.read()
+
+tags_metadata = [
+    {
+        "name": "login",
+        "description": "Login and Signup",
+    },
+    {
+        "name": "sum",
+        "description": "Student User CRUD operations",
+    },
+    {
+        "name": "aum",
+        "description": "Admin User CRUD operations",
+    },
+    {
+        "name": "suu",
+        "description": "Student User Manipulation",
+    }
+]
+
+# API
+
+app = FastAPI(
+    openapi_tags=tags_metadata,
+    redoc_url=None,
+    title="StudentlyAPI",
+    description=description,
+    summary="Studently Backend API",
+    version="0.0.1",
+    contact={
+        "name": CONTACT_NAME,
+        "emial": CONTACT_EMAIL,
+    }
+)
 
 
-@app.post("/signup", response_model=Student)
+# Login Endpoints
+
+@app.post("/signup", tags=["login"], response_model=Student)
 async def create_user(data: Student):
     # Query Database to check if user exists
 
@@ -43,8 +95,7 @@ async def create_user(data: Student):
             detail=f"User {data.email} already exists"
         )
 
-
-@app.post('/login')
+@app.post('/login', tags=["login"])
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = db.read_student("username", form_data.username)
     if user is None:
@@ -67,12 +118,9 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     }
 
 
-@app.get("/me")
-async def get_me(user = Depends(sec.get_current_user)):
-    return user
+# Student manipulation Endpoints
 
-
-@app.post("/createStudent", response_model=Student)
+@app.post("/manstudent", tags=["sum"], response_model=Student)
 async def create_student(data: Student):
     # Query Database to check if user exists
 
@@ -99,16 +147,48 @@ async def create_student(data: Student):
         )
 
 
-@app.get("/getStudent", response_model=Student)
+@app.get("/manstudent", tags=["sum"], response_model=Student)
 async def get_student(data: Student):
     pass
 
 
-@app.get("updateStudent", response_model=Student)
+@app.put("manstudent", tags=["sum"], response_model=Student)
 async def update_student(data: Student):
     pass
 
 
-@app.delete("/deleteStudent", response_model=Student)
+@app.delete("/manstudent", tags=["sum"], response_model=Student)
 async def delete_student(data: Student):
     pass
+
+
+# Admin manipulation Endpoints
+
+@app.post("/manadmin", tags=["aum"])
+async def create_admin():
+    pass
+
+@app.get("/manadmin", tags=["aum"])
+async def get_admin():
+    pass
+
+@app.put("/manadmin", tags=["aum"])
+async def update_admin():
+    pass
+
+@app.delete("/manadmin", tags=["aum"])
+async def delete_admin():
+    pass
+
+
+# Studen User Endpoints
+
+@app.get("/me", tags=["suu"])
+async def get_me(user = Depends(sec.get_current_user)):
+    # Authentification Check Example
+    if isinstance(user, Student):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"User {user.email} is not authorized for this endpoint"
+        )
+    return user
