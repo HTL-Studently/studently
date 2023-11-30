427d867b7b5241385e83d9b4188944567d8d6fe7
kunospan <kunosp@edu.htl-villach.at>
2023-11-30 21:29:52 +0000

    Created intial UI

--- frontend/src/components/header.svelte
+++ frontend/src/components/header.svelte
@@ -1,7 +1,33 @@
-<script>
+<style>
+	header{
+		margin-left: 15%;
+		width: 85%;
+		min-height: 5vh;
+		background-color: #fff;
+		text-align: right;
+        position: fixed;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		
+	}
+	header img{
+		max-width: 2em;
+	}
+	header p{
 
-</script>
+		margin: 1.5em;
+		font-size: large;
+		display: flex;
+		align-items: center;
 
-<div class="component">
-    <h1 class="text-blue-400">Header</h1>
-</div>
+	}
+</style>
+
+<header>
+	<button>
+		<img src="../public/logout.png" alt="Logout">
+	</button>
+	
+	<p>Max Mustermann, 5BHIF</p>
+</header>
---
+++ frontend/src/components/navbar.svelte
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<style>
+	ul li{
+		text-decoration: none;
+		color: #000; 
+		padding: 10px;
+		transition: background-color 0.3s ease;
+		font-size: 1.5em;
+		width: 100%;
+		
+	}
+	ul li:hover{
+		background-color: #fbbf06;
+		text-decoration: none;
+		color: #000; 
+		padding: 10px;
+		border-radius: 10px;
+		
+	}
+	nav {
+		height: 100vh;
+		width: 15%;
+		background-color: #ffe65d;
+		position: fixed;
+		top: 0;
+		left: 0;
+		display: flex;
+		flex-direction: column; 
+		padding: 20px;
+	}
+	ul{
+		margin-top: 10vh;
+		width: 100%;
+		
+	}
+	img{
+		align-items: center;
+		margin-left: -1em;
+	}
+	
+</style>
+<nav>
+	<img src="../public/logo.png" alt="Logo"/>
+	<ul>
+		<li><a href="/pages/index" >Overview</a></li>
+		<li><a href="licenses.astro" >Licenses</a></li> 
+		<li><a href="../pages/payments.astro" >Payments</a></li>
+	</ul>
+</nav>
+
+
+
