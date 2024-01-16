9da55fb4b2e928dd4ded8ec64d8a4f5341d1bc61
eriksimcic <lionmaster23@protonmail.com>
2024-01-16 20:30:42 +0000

    Started adding dockerization for the frontend

--- sveltekit-frontend/svelte.config.js
+++ sveltekit-frontend/svelte.config.js
@@ -3,10 +3,16 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	vitePlugin: {
+		experimental: {
+			// Allows you to hold ctrl+shift and click on an item in the browser and it then opens that components location in VSCode
+			inspector: {
+				holdMode: true
+			},
+		},
 	}
 };
 
