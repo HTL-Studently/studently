580c9fdaea1d8fed5b245721903a1862e3bcfb46
kunospan <kunosp@edu.htl-villach.at>
2024-02-20 19:01:24 +0000

    20.02.2024

--- sveltekit-frontend/src/routes/(protected)/licenses/+page.svelte
+++ sveltekit-frontend/src/routes/(protected)/licenses/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Licenses</h1>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let cookieContent = "";
+
+    onMount(() => {
+
+    });
+
+    import Licenses from '$lib/parent_licenses.svelte';
+
+    function checkAlive() {
+        alert("Still living");
+    }
+
+</script>
+
+<div class=" h-screen ">
+    <!-- <h1>Licenses</h1> -->
+    <div class="flex flex-1 justify-center items-center">
+    <Licenses/>
+    </div>
+</div>
--- sveltekit-frontend/src/routes/(protected)/payments/+page.svelte
+++ sveltekit-frontend/src/routes/(protected)/payments/+page.svelte
@@ -1,1 +1,24 @@
-<h1>Payments</h1>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let cookieContent = "";
+
+    onMount(() => {
+
+    });
+
+    import Payments from "$lib/parent_payments.svelte";
+
+    function checkAlive() {
+        alert("Still living");
+    }
+
+</script>
+
+<div class=" h-screen ">
+    <!-- <h1>Payments</h1> -->
+
+    <div class="flex flex-1 justify-center items-center mt-10">
+        <Payments/>
+    </div>
+</div>
